3a86cf02d8a836e6f05f1c159aeb820d930a8d41
Leonardo Tononi <easaaa@protonmail.com>
2019-01-19 11:58:57 +0100

    fixed problem with img slide code

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// SLIDE IMG INDEX PAGE. ---  CONFLICT
+
+var slide_index = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slide_index++;
+  if (slide_index > x.length) {
+    slide_index = 1
+  }
+  x[slide_index - 1].style.display = "block";
+  setTimeout(carousel, 3000); 
+}
+
 
 $(document).ready(function () {
   
