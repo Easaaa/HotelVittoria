f81de39cb4633a926d88ebc851a9b53b79fffe17
Leonardo Tononi <easaaa@protonmail.com>
2019-04-14 12:03:38 +0200

    Change style index-rooms

--- javascript/appPrices.js
+++ javascript/appPrices.js
@@ -1,37 +1,14 @@
-// DROP DOWN MENU
-
-$(".dropdown-selection").click(function() {
-  $(this)
-    .attr("tabindex", 1)
-    .focus();
-  $(this).toggleClass("active");
-  $(this)
-    .find(".dropdown-menu-selection")
-    .slideToggle(300);
-});
-$(".dropdown-selection").focusout(function() {
-  $(this).removeClass("active");
-  $(this)
-    .find(".dropdown-menu-selection")
-    .slideUp(300);
-});
-$(".dropdown-selection .dropdown-menu-selection li").click(function() {
-  $(this)
-    .parents(".dropdown-selection")
-    .find("span")
-    .text($(this).text());
-  $(this)
-    .parents(".dropdown-selection")
-    .find("input")
-    .attr("value", $(this).attr("id"));
-});
-
 // INPUT
 const input = document.querySelectorAll("input");
-const inputArr = [input[0], input[1], input[2]];
-const inputSeason = input[0];
-const inputPerson = input[1];
-const inputView = input[2];
+const highOption = input[0].id;
+const lowOption = input[1].id;
+const singleOption = input[2].id;
+const doubleOption = input[3].id;
+const tripleOption = input[4].id;
+const quadrupleOption = input[5].id;
+const lakeOption = input[6].id;
+const gardenOption = input[7].id;
+const mountainOption = input[8].id;
 
 // RESULTS VARIABLE
 const img = document.querySelector(".dinamic-img");
@@ -45,20 +22,7 @@ const loader_id = document.getElementById("loading");
 // FEEDBACK MSG
 const error_id = document.getElementById("error");
 
-// RESET INPUT
-const selectInput_div = document.querySelectorAll(".select");
-const selectInputSeason = selectInput_div[0];
-const selectInputPerson = selectInput_div[1];
-const selectInputView = selectInput_div[2];
-
-// CLEAR INPUT
-function clearInput() {
-  selectInputSeason.innerHTML = "<span>Scegli la stagione</span>";
-  selectInputPerson.innerHTML = "<span>Quante persone?</span>";
-  selectInputView.innerHTML = "<span>Scegli il tipo di camera</span>";
-}
-
-// HIDE ALL
+/* // HIDE ALL
 function cleanAll() {
   error_id.style.display = "none";
   results_div.style.visibility = "hidden";
@@ -74,274 +38,269 @@ function loading() {
 function feedToError() {
   loader_id.style.display = "none";
   error_id.style.display = "block";
-}
+} */
 
 // LISTEN FOR SUBMIT
-document.getElementById("show-btn").addEventListener("click", searchOn);
+document.getElementById("search-btn").addEventListener("click", showRooms);
+
+/* document.getElementById("reset-btn").addEventListener("click", resetPage); */
 
 // FUNCTION SUBMIT
-function searchOn(e) {
+function showRooms(e) {
   e.preventDefault();
-  cleanAll();
-  loading();
-  setInterval(function() {
-    if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "single" &&
-      inputView.value === "lake-view"
-    ) {
-      feedToError();
-      setInterval(function() {
-        error_id.style.display = "none";
-        location.reload();
-      }, 3000);
-    }
-    if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "double" &&
-      inputView.value === "lake-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index2.png");
-      numberPeople.textContent = "2";
-      price.textContent = "€ 100";
-      results_div.style.visibility = "visible";
-      clearInput();
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "triple" &&
-      inputView.value === "lake-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index2.png");
-      numberPeople.textContent = "3";
-      price.textContent = "€ 130";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "quadruple" &&
-      inputView.value === "lake-view"
-    ) {
-      feedToError();
-      setInterval(function() {
-        error_id.style.display = "none";
-        location.reload();
-      }, 3000);
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "single" &&
-      inputView.value === "garden-view"
-    ) {
-      feedToError();
-      setInterval(function() {
-        error_id.style.display = "none";
-        location.reload();
-      }, 3000);
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "double" &&
-      inputView.value === "garden-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index.jpg");
-      viewWord.textContent = "giardino";
-      numberPeople.textContent = "2";
-      price.textContent = "€ 96";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "triple" &&
-      inputView.value === "garden-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index.jpg");
-      viewWord.textContent = "giardino";
-      numberPeople.textContent = "3";
-      price.textContent = "€ 125";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "quadruple" &&
-      inputView.value === "garden-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index.jpg");
-      viewWord.textContent = "giardino";
-      numberPeople.textContent = "4";
-      price.textContent = "€ 160";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "single" &&
-      inputView.value === "mountain-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index1.jpg");
-      viewWord.textContent = "montagna";
-      numberPeople.textContent = "1";
-      price.textContent = "€ 50";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "double" &&
-      inputView.value === "mountain-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index1.jpg");
-      viewWord.textContent = "montagna";
-      numberPeople.textContent = "2";
-      price.textContent = "€ 96";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "triple" &&
-      inputView.value === "mountain-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index1.jpg");
-      viewWord.textContent = "montagna";
-      numberPeople.textContent = "3";
-      price.textContent = "€ 110";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "high-season" &&
-      inputPerson.value === "quadruple" &&
-      inputView.value === "mountain-view"
-    ) {
-      feedToError();
-      setInterval(function() {
-        error_id.style.display = "none";
-        location.reload();
-      }, 3000);
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "single" &&
-      inputView.value === "lake-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index2.png");
-      numberPeople.textContent = "1";
-      price.textContent = "€ 76.50";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "double" &&
-      inputView.value === "lake-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index2.png");
-      numberPeople.textContent = "2";
-      price.textContent = "€ 90";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "triple" &&
-      inputView.value === "lake-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index2.png");
-      numberPeople.textContent = "3";
-      price.textContent = "€ 120";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "quadruple" &&
-      inputView.value === "lake-view"
-    ) {
-      feedToError();
-      setInterval(function() {
-        error_id.style.display = "none";
-        location.reload();
-      }, 3000);
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "single" &&
-      inputView.value === "garden-view"
-    ) {
-      feedToError();
-      setInterval(function() {
-        error_id.style.display = "none";
-        location.reload();
-      }, 3000);
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "double" &&
-      inputView.value === "garden-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index.jpg");
-      viewWord.textContent = "giardino";
-      numberPeople.textContent = "2";
-      price.textContent = "€ 88";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "triple" &&
-      inputView.value === "garden-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index.jpg");
-      viewWord.textContent = "giardino";
-      numberPeople.textContent = "3";
-      price.textContent = "€ 115";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "quadruple" &&
-      inputView.value === "garden-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index.jpg");
-      viewWord.textContent = "giardino";
-      numberPeople.textContent = "4";
-      price.textContent = "€ 140";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "single" &&
-      inputView.value === "mountain-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index1.jpg");
-      viewWord.textContent = "montagna";
-      numberPeople.textContent = "1";
-      price.textContent = "€ 45";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "double" &&
-      inputView.value === "mountain-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index1.jpg");
-      viewWord.textContent = "montagna";
-      numberPeople.textContent = "2";
-      price.textContent = "€ 86";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "triple" &&
-      inputView.value === "mountain-view"
-    ) {
-      cleanAll();
-      img.setAttribute("src", "../images/rooms-index1.jpg");
-      viewWord.textContent = "montagna";
-      numberPeople.textContent = "3";
-      price.textContent = "€ 100";
-      results_div.style.visibility = "visible";
-    } else if (
-      inputSeason.value === "low-season" &&
-      inputPerson.value === "quadruple" &&
-      inputView.value === "mountain-view"
-    ) {
-      feedToError();
-      setInterval(function() {
-        error_id.style.display = "none";
-      }, 3000);
-    }
-    /* else feedToError();
+  /* cleanAll(); */
+  /* loading(); */
+  if (
+    highOption &&
+    singleOption &&
+    lakeOption
+  ) {
+    /* feedToError(); */
     setInterval(function () {
       error_id.style.display = "none";
-    }, 3000); */
-  }, 2000);
+      location.reload();
+    }, 3000);
+  }
+  if (
+    highOption &&
+    doubleOption &&
+    lakeOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index2.png");
+    numberPeople.textContent = "2";
+    price.textContent = "€ 100";
+    results_div.style.visibility = "visible";
+  } else if (
+    highOption &&
+    tripleOption &&
+    lakeOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index2.png");
+    numberPeople.textContent = "3";
+    price.textContent = "€ 130";
+    results_div.style.visibility = "visible";
+  } else if (
+    highOption &&
+    quadrupleOption &&
+    lakeOption
+  ) {
+    /* feedToError(); */
+    setInterval(function () {
+      error_id.style.display = "none";
+      location.reload();
+    }, 3000);
+  } else if (
+    highOption &&
+    singleOption &&
+    gardenOption
+  ) {
+    /* feedToError(); */
+    setInterval(function () {
+      error_id.style.display = "none";
+      location.reload();
+    }, 3000);
+  } else if (
+    highOption &&
+    doubleOption &&
+    gardenOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index.jpg");
+    viewWord.textContent = "giardino";
+    numberPeople.textContent = "2";
+    price.textContent = "€ 96";
+    results_div.style.visibility = "visible";
+  } else if (
+    highOption &&
+    tripleOption &&
+    gardenOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index.jpg");
+    viewWord.textContent = "giardino";
+    numberPeople.textContent = "3";
+    price.textContent = "€ 125";
+    results_div.style.visibility = "visible";
+  } else if (
+    highOption &&
+    quadrupleOption &&
+    gardenOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index.jpg");
+    viewWord.textContent = "giardino";
+    numberPeople.textContent = "4";
+    price.textContent = "€ 160";
+    results_div.style.visibility = "visible";
+  } else if (
+    highOption &&
+    singleOption &&
+    mountainOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index1.jpg");
+    viewWord.textContent = "montagna";
+    numberPeople.textContent = "1";
+    price.textContent = "€ 50";
+    results_div.style.visibility = "visible";
+  } else if (
+    highOption &&
+    doubleOption &&
+    mountainOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index1.jpg");
+    viewWord.textContent = "montagna";
+    numberPeople.textContent = "2";
+    price.textContent = "€ 96";
+    results_div.style.visibility = "visible";
+  } else if (
+    highOption &&
+    tripleOption &&
+    mountainOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index1.jpg");
+    viewWord.textContent = "montagna";
+    numberPeople.textContent = "3";
+    price.textContent = "€ 110";
+    results_div.style.visibility = "visible";
+  } else if (
+    highOption &&
+    quadrupleOption &&
+    mountainOption
+  ) {
+    /* feedToError(); */
+    setInterval(function () {
+      error_id.style.display = "none";
+      location.reload();
+    }, 3000);
+  } else if (
+    lowOption &&
+    singleOption &&
+    lakeOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index2.png");
+    numberPeople.textContent = "1";
+    price.textContent = "€ 76.50";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    doubleOption &&
+    lakeOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index2.png");
+    numberPeople.textContent = "2";
+    price.textContent = "€ 90";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    tripleOption &&
+    lakeOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index2.png");
+    numberPeople.textContent = "3";
+    price.textContent = "€ 120";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    quadrupleOption &&
+    lakeOption
+  ) {
+    /* feedToError(); */
+    setInterval(function () {
+      error_id.style.display = "none";
+      location.reload();
+    }, 3000);
+  } else if (
+    lowOption &&
+    singleOption &&
+    gardenOption
+  ) {
+    /* feedToError(); */
+    setInterval(function () {
+      error_id.style.display = "none";
+      location.reload();
+    }, 3000);
+  } else if (
+    lowOption &&
+    doubleOption &&
+    gardenOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index.jpg");
+    viewWord.textContent = "giardino";
+    numberPeople.textContent = "2";
+    price.textContent = "€ 88";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    tripleOption &&
+    gardenOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index.jpg");
+    viewWord.textContent = "giardino";
+    numberPeople.textContent = "3";
+    price.textContent = "€ 115";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    quadrupleOption &&
+    gardenOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index.jpg");
+    viewWord.textContent = "giardino";
+    numberPeople.textContent = "4";
+    price.textContent = "€ 140";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    singleOption &&
+    mountainOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index1.jpg");
+    viewWord.textContent = "montagna";
+    numberPeople.textContent = "1";
+    price.textContent = "€ 45";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    doubleOption &&
+    mountainOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index1.jpg");
+    viewWord.textContent = "montagna";
+    numberPeople.textContent = "2";
+    price.textContent = "€ 86";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    tripleOption &&
+    mountainOption
+  ) {
+    /* cleanAll(); */
+    img.setAttribute("src", "../images/rooms-index1.jpg");
+    viewWord.textContent = "montagna";
+    numberPeople.textContent = "3";
+    price.textContent = "€ 100";
+    results_div.style.visibility = "visible";
+  } else if (
+    lowOption &&
+    quadrupleOption &&
+    mountainOption
+  ) {
+    /* feedToError(); */
+    setInterval(function () {
+      error_id.style.display = "none";
+    }, 3000);
+  }
 }
